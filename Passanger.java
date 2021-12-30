e335a922840baa8fa020de23b3a98f9f83c3b503
vinithaManchuri <95231857+vinithaManchuri@users.noreply.github.com>
2021-12-30 20:50:37 +0530

    Add files via upload

---
+++ Passanger.java
@@ -0,0 +1,16 @@
+public class Passenger {
+
+    String bookTicket;
+    boolean  availableOfFlight;
+
+    public static void main(String [] args)  {
+
+        Passenger P1 = new Passenger ();
+        System.out.println(P1.bookTicket);
+
+        Passenger P2 = new Passenger ();
+        System.out.println(P2.availableOfFlight);
+
+
+    }
+}
---
+++ TouristTicket.java
@@ -0,0 +1,16 @@
+public class TouristTicket {
+
+    String hotelService;
+    String touristLocation;
+
+    public static void main(String[] args) {
+
+        TouristTicket T1 = new TouristTicket();
+        System.out.println(T1.hotelService);
+
+        TouristTicket T2 = new TouristTicket();
+        System.out.println(T2.touristLocation);
+
+    }
+}
+
