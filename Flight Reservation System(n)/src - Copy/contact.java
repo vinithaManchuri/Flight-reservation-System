6ebb04dc6b0c17b5a7df4629655ffb78b8625c70
vinithaManchuri <95231857+vinithaManchuri@users.noreply.github.com>
2021-12-31 00:08:13 +0530

    Add files via upload

---
+++ Flight Reservation System(n)/src - Copy/contact.java
@@ -0,0 +1,19 @@
+public class Contact {
+
+    String  name;
+    int phoneNumber;
+    String emailId;
+
+    public static void main(String []args){
+
+        Contact C1 = new Contact ();
+        System.out.println(C1.name);
+
+        Contact C2 = new Contact ();
+        System.out.println(C2.phoneNumber);
+
+        Contact C3 = new Contact ();
+        System.out.println(C3.emailId);
+
+    }
+}
---
+++ Flight Reservation System(n)/src/RegularTicket.java
@@ -0,0 +1,21 @@
+public class RegularTicket {
+
+    String food;
+    String water;
+    String snacks;
+
+    public static void main(String [] args)  {
+
+        RegularTicket R1 = new RegularTicket ();
+        System.out.println(R1.food);
+
+        RegularTicket R2 = new RegularTicket ();
+        System.out.println(R2.water);
+
+        RegularTicket R3 = new RegularTicket ();
+        System.out.println(R3.snacks);
+
+
+    }
+
+}
