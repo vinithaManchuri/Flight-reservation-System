c84119db066b3effd56df7815536516a547dc4d8
vinithaManchuri <95231857+vinithaManchuri@users.noreply.github.com>
2021-12-31 22:38:54 +0530

    Add files via upload

---
+++ Flight Resevation System(check point 2)/Flight Reservation System(n)/src/Address.java
@@ -0,0 +1,18 @@
+public class Address {
+    String street;
+    String city;
+    String state;
+
+    public static void main(String []args){
+
+        Address A1 = new Address ();
+        System.out.println(A1.street);
+
+        Address A2 = new Address ();
+        System.out.println(A2.city);
+
+        Address A3 = new Address ();
+        System.out.println(A3.state);
+
+    }
+}
